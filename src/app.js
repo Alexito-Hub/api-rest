2130d3d269e9ae016a68889add80aefa26029c72
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-18 14:07:01 -0500

    update app.js

--- src/app.js
+++ src/app.js
@@ -6,15 +6,15 @@ const port = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
 
-function CONNECTION_DB(dbname) {
-    return `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.ncdk5.mongodb.net/${dbname}?retryWrites=true&w=majority`;
-}
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.ncdk5.mongodb.net/api-rest?retryWrites=true&w=majority`;
 
-mongoose.connect(CONNECTION_DB('api-rest'),
+mongoose.connect(uri,
     { useNewUrlParser: true, useUnifiedTopology: true }
 )
     .then(() => console.log('Base de datos conectada'))
     .catch(e => console.log(e))
+
+
 
 app.use('/api/users', require('./routers/users'));
 
