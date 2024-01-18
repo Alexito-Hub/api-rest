57284b48adf0aedbdd678ed67cbb1cac9bcd80cc
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-18 15:03:55 -0500

    update app.js

--- src/app.js
+++ src/app.js
@@ -6,13 +6,21 @@ const port = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
 
-const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0-OMITTED.mongodb.net/` + `api-rest?retryWrites=true&w=majority`;
-// Prints "MongoServerError: bad auth Authentication failed."
-mongoose.connect(uri, {
-    serverSelectionTimeoutMS: 5000
-})
-.then(() => console.log('Base de datos conectada'))
-.catch(e => console.log(e))
+
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.mongodb.net/api-rest?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conexión a MongoDB exitosa');
+  })
+  .catch((err) => {
+    console.error('Error conectando a MongoDB:', err);
+  });
+
+
+
+
 
 
 app.use('/api/users', require('./routers/users'));
