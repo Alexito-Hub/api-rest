b8c453f54fa9884d4adff295a6b7ea10a92fac35
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-18 14:52:55 -0500

    update app.js

--- src/app.js
+++ src/app.js
@@ -6,14 +6,11 @@ const port = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
 
-const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.ncdk5.mongodb.net/api-rest?retryWrites=true&w=majority`;
-
-mongoose.connect(uri,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-)
-    .then(() => console.log('Base de datos conectada'))
-    .catch(e => console.log(e))
-
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0-OMITTED.mongodb.net/` + `api-rest?retryWrites=true&w=majority`;
+// Prints "MongoServerError: bad auth Authentication failed."
+mongoose.connect(uri, {
+  serverSelectionTimeoutMS: 5000
+}).catch(err => console.log(err.reason));
 
 
 app.use('/api/users', require('./routers/users'));
