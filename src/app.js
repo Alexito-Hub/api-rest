19198266f5c1fdec8429686f01284ddd88873dbd
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-18 20:27:50 -0500

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,30 +1,19 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
-const app = express();
-
-const port = process.env.PORT || 3000;
-
 const mongoose = require('mongoose');
 
-
+const app = express();
+const port = process.env.PORT || 3000;
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.mongodb.net/api-rest?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Conexión a MongoDB exitosa');
-  })
-  .catch((err) => {
-    console.error('Error conectando a MongoDB:', err);
-  });
+  .then(() => console.log('Conexión a MongoDB exitosa'))
+  .catch(err => console.error('Error conectando a MongoDB:', err));
 
+// Asegúrate de que mongoose esté conectado antes de escuchar en el puerto
+mongoose.connection.on('connected', () => {
+  app.use('/api/users', require('./routers/users'));
 
-
-
-
-
-app.use('/api/users', require('./routers/users'));
-
-app.listen(port, () => {
-    console.log('Servidor en linea')
-})
+  app.listen(port, () => console.log(`Servidor en línea en el puerto ${port}`));
+});
