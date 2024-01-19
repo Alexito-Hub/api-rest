07e3b93b28386643bab37d9c87ba7cf38231db5c
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-18 21:05:05 -0500

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const uri = `mongodb+srv://<span class="math-inline">\{process\.env\.USER\_DB\}\:</span>{process.env.PASSWORD_DB}@cluster0.mongodb.net/api-rest?retryWrites=true&w=majority`;
+const MongoClient = require("mongodb");
 
-mongoose.connect(uri)
-  .then(() => console.log('Conexión a MongoDB exitosa'))
-  .catch(err => console.error('Error conectando a MongoDB:', err));
+const client = new MongoClient("mongodb+srv://myusername:mypassword@mycluster.mongodb.net/mydb");
 
-mongoose.connection.on('connected', () => {
-  app.use('/api/users', require('./routers/users'));
-
-  app.listen(port, () => console.log(`Servidor en línea en el puerto ${port}`));
+client.connect((err, db) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Conectado a la base de datos");
+  }
 });
+
+
+
+app.use('/api/users', require('./routers/users'));
+app.listen(port, () => console.log(`Servidor en línea en el puerto ${port}`));
