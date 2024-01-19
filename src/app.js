8048fdab4d9b2a308bee63ba0d5c440fc99ac166
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-18 21:08:28 -0500

    update app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 const MongoClient = require("mongodb");
 
-const client = new MongoClient("mongodb+srv://myusername:mypassword@mycluster.mongodb.net/mydb");
+const client = new MongoClient(`mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.mongodb.net/api-rest`);
 
 client.connect((err, db) => {
   if (err) {
