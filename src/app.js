2fe5de72993f5a357e6f5249488f6edf0483367f
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-25 16:36:03 -0500

    update app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 3000;
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://<span class="math-inline">\{process\.env\.USER\_DB\}\:</span>{process.env.PASSWORD_DB}@serverdatadb.39fv13g.mongodb.net/?retryWrites=true&w=majority`;
+const encodedPassword = encodeURIComponent(process.env.PASSWORD_DB);
+const uri = `mongodb+srv://<span class="math-inline">\{process\.env\.USER\_DB\}\:</span>{encodedPassword}@serverdatadb.39fv13g.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {
